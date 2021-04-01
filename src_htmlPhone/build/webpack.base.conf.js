80d8da1cf3859c7a8181d6262c41a09b52f70ea4
Franco Sanllehi <francosanllehi@gmail.com>
2021-04-01 00:04:32 -0300

    Webpack
    Webpack 4 -> 5
    Faster compilation (60s -> 37s)
    Optimize Images

--- src_htmlPhone/build/webpack.base.conf.js
+++ src_htmlPhone/build/webpack.base.conf.js
@@ -60,30 +60,6 @@ module.exports = {
               name: utils.assetsPath('img/[name].[ext]')
             }
           },
-          {
-            loader: 'img-loader',
-            options: {
-              plugins: [
-                require('imagemin-gifsicle')({
-                  interlaced: false
-                }),
-                require('imagemin-mozjpeg')({
-                  progressive: true,
-                  arithmetic: false
-                }),
-                require('imagemin-pngquant')({
-                  floyd: 0.5,
-                  speed: 2
-                }),
-                require('imagemin-svgo')({
-                  plugins: [
-                    { removeTitle: true },
-                    { convertPathData: false }
-                  ]
-                })
-              ]
-            }
-          }
         ]
       },
       {
--- src_htmlPhone/build/webpack.dev.conf.js
+++ src_htmlPhone/build/webpack.dev.conf.js
@@ -4,7 +4,6 @@ const config = require('../config')
 const { merge } = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -27,6 +26,5 @@ module.exports = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true
     }),
-    new FriendlyErrorsPlugin()
   ]
 })
